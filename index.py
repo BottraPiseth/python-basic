f5b7ee01f2fe3909b18d81daabe6d81aee81dfda
seang piseth <piseth1682014@gmail.com>
2023-10-10 20:13:36 +0700

    Initial commit

---
+++ index.py
@@ -0,0 +1,27 @@
+print('hello')
+name=234234
+# print(x)
+z=100100
+print('name',+z)
+print(name,+z)
+# a1=("input your name :" )
+# print(a1)
+# a= input()
+string1 = "hello"
+string2 = "how are u "
+
+new_string = string1 + " " + string2
+print(new_string)
+#datatype
+x=1 + 1
+#data conversion
+e3= 1+ 2.2
+print(str(e3) + 'hello' )
+answerPrompt = 'your answer is :'
+print(answerPrompt + str(e3))
+#or
+yi= int(input('Enter a number:'))
+print(type(yi))
+# print(int(yi) + 2)
+print(yi + 2)
+#string
