482b7bd0d29ecd02454230d6c53d9eb061676740
seang piseth <piseth1682014@gmail.com>
2023-10-11 18:02:10 +0700

    Initial commit

--- index.py
+++ index.py
@@ -20,8 +20,55 @@ print(str(e3) + 'hello' )
 answerPrompt = 'your answer is :'
 print(answerPrompt + str(e3))
 #or
-yi= int(input('Enter a number:'))
-print(type(yi))
+# yi= int(input('Enter a number:'))
+# print(type(yi))
 # print(int(yi) + 2)
-print(yi + 2)
+# print(yi + 2)
 #string
+print('hello\t\tworld')
+print('Is\'my name')
+qa = 'hello'
+wa = 'world'
+print(qa+' '+wa)
+# function
+namev = 'PISETH'
+print(namev.lower())
+
+namey = 'piseth'
+print(namev.upper())
+
+print(len(namev))
+
+print(namev[2::])
+
+print(namev[::-1])
+
+print(namey[0].lower() + namey[1::] )
+
+print(round(0.966)) 
+
+#boolean true or false
+
+user = "user"
+
+if (user == "admin1"):
+    print("correct")
+elif(user == "user"):
+    print('Is\'s user')
+else:
+    print('user unknow')
+
+num = 11
+
+if (num == 10):
+    print("correct")
+elif (num > 10):
+    print('Num is greater than 10')
+else:
+      print('num is less than 10')    
+# or 
+num2 = 10
+if (num2 >= 20):
+    print("number is good enough")
+else:
+    print("number is too small")
